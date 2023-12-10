aa0adcb26e9ad6db251dc38c15667068a8eaa4cf
saeedjamali89@gmail.com <saeedjamali89@gmail.com>
2023-12-10 21:56:26 +0330

    Define CategoryView Js

---
+++ src/js/CategoryView.js
@@ -0,0 +1,6 @@
+const categoryTitle = document.querySelector("#category__title");
+const categoryDescription = document.querySelector("#category__description");
+const categoryNew = document.querySelector("#category__new");
+
+
+
